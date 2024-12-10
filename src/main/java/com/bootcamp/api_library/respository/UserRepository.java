5ef1263141b0098783a62535bc372a4fc37ce0cc
dustisme <androsrivas@outlook.com>
2024-12-10 21:18:36 +0100

    create user service with getAll method

--- src/main/java/com/bootcamp/api_library/respository/UserRepository.java
+++ src/main/java/com/bootcamp/api_library/respository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.bootcamp.api_library.respository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository {
 }
