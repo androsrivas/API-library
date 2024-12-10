2b114a8874b3f97bb7bdffdd5976ac470e256ae2
dustisme <androsrivas@outlook.com>
2024-12-10 21:27:20 +0100

    correct user repository mistake

--- src/main/java/com/bootcamp/api_library/respository/UserRepository.java
+++ src/main/java/com/bootcamp/api_library/respository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.bootcamp.api_library.respository;
 
+import com.bootcamp.api_library.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository <User, Long> {
 }
