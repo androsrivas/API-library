8af3a3fef6818addf16eecb2da9532a876bafcfc
dustisme <androsrivas@outlook.com>
2024-12-10 12:24:39 +0100

    initial commit

---
+++ src/main/java/com/bootcamp/api_library/ApiLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.api_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/api_library/ApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.api_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
