119f7d1cb31d131b8af01c25c08235ef75741567
dustisme <androsrivas@outlook.com>
2024-12-11 13:06:19 +0100

    create user repository

--- src/main/java/com/bootcamp/api_library/respository/BookRepository.java
+++ src/main/java/com/bootcamp/api_library/respository/BookRepository.java
@@ -2,19 +2,19 @@ package com.bootcamp.api_library.respository;
 
 import com.bootcamp.api_library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query("SELECT b FROM Book b WHERE b.title LIKE %?1%")
-    Book findBookByTitleContaining(String title);
+    Optional<Book> findBookById(UUID id);
 
-    @Query("SELECT b FROM Book b WHERE ?1 MEMBER OF b.authors")
-    List<Book> findBooksByAuthorsContaining(String author);
+    Optional<Book> findBookByTitleContaining(String title);
 
-    @Query("SELECT b FROM Book b WHERE ?1 MEMBER OF b.genres")
-    List<Book> findBooksByGenresContaining(String genre);
+    Optional<List<Book>> findBooksByAuthors(String author);
+
+    Optional<List<Book>> findBooksByGenres(String genre);
 }
