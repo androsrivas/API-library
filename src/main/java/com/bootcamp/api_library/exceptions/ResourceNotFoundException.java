10c893939f325e939609ec520761eb111930e4dc
dustisme <androsrivas@outlook.com>
2024-12-16 11:49:08 +0100

    refactor book repository

---
+++ src/main/java/com/bootcamp/api_library/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bootcamp.api_library.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
