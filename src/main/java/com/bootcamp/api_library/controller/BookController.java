44673c4daed8181f407d7c55be59984a62fa27df
dustisme <androsrivas@outlook.com>
2024-12-16 12:49:47 +0100

    refactor Book controller

--- src/main/java/com/bootcamp/api_library/controller/BookController.java
+++ src/main/java/com/bootcamp/api_library/controller/BookController.java
@@ -1,6 +1,9 @@
 package com.bootcamp.api_library.controller;
 
 import com.bootcamp.api_library.model.Book;
+import com.bootcamp.api_library.service.Book.BookByAuthorService;
+import com.bootcamp.api_library.service.Book.BookByGenreService;
+import com.bootcamp.api_library.service.Book.BookByTitleService;
 import com.bootcamp.api_library.service.Book.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,45 +14,40 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/v1/books")
 public class BookController {
     private final BookService bookService;
+    private final BookByTitleService bookByTitleService;
+    private final BookByAuthorService bookByAuthorService;
+    private final BookByGenreService bookByGenreService;
 
-    public BookController(BookService bookService) {
+
+    public BookController(BookService bookService, BookByTitleService bookByTitleService, BookByAuthorService bookByAuthorService, BookByGenreService bookByGenreService) {
         this.bookService = bookService;
+        this.bookByTitleService = bookByTitleService;
+        this.bookByAuthorService = bookByAuthorService;
+        this.bookByGenreService = bookByGenreService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> create(@RequestBody Book newBook) {
+        return ResponseEntity.status(201).body(bookService.createBook(newBook));
     }
 
     @GetMapping("/books")
-    public List<Book> getAllBooks() {
+    public List<Book> getAll() {
         return bookService.getAllBooks();
     }
 
-    @GetMapping("/books/{id}")
-    public Optional<Book> getBookById(@PathVariable UUID id) {
-        return bookService.getBookById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getById(@PathVariable UUID id) {
+        return bookService.getBookById(id)
+                .map(book -> ResponseEntity.ok(book))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/books/title")
-    public Optional<List<Book>> getBookByTitle(@RequestParam String title) {
-        return bookService.getBookByTitle(title);
-    }
-
-    @GetMapping("/books/author")
-    public Optional<List<Book>> getBooksByAuthors(@RequestParam String author) {
-        return bookService.getBooksByAuthor(author);
-    }
-
-    @GetMapping("/books/genre")
-    public Optional<List<Book>> getBooksByGenre(@RequestParam String genre) {
-        return bookService.getBooksByGenre(genre);
-    }
-
-    @PostMapping("/books")
-    public void createBook(@RequestBody Book newBook) {
-        bookService.addBook(newBook);
-    }
-
-    @PutMapping("/books/{id}")
-    public ResponseEntity<Book> updateBook(@PathVariable UUID id, @RequestBody Book bookDetails) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> update(@PathVariable UUID id, @RequestBody Book bookDetails) {
         try {
             Book book = bookService.updateBook(id, bookDetails);
             return new ResponseEntity<>(book, HttpStatus.OK);
@@ -58,8 +56,19 @@ public class BookController {
         }
     }
 
-    @DeleteMapping("/books/{id}")
-    public void deleteBook(@PathVariable UUID id) {
-        bookService.deleteBook(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        if (bookService.deleteBook(id)) {
+            return ResponseEntity.noContent().build();
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<Book>> getByTitle(@PathVariable String title) {
+        List<Book> books = bookByTitleService.findByTitle(title);
+        return books.isEmpty()
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(books);
     }
 }
