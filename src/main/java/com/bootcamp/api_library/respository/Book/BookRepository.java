43fc814f27af37e7f8ed38d9c1acfbb3b8a5d69a
dustisme <androsrivas@outlook.com>
2024-12-16 13:54:27 +0100

    adapt BookSearch service to specifications

--- src/main/java/com/bootcamp/api_library/respository/Book/BookRepository.java
+++ src/main/java/com/bootcamp/api_library/respository/Book/BookRepository.java
@@ -2,10 +2,11 @@ package com.bootcamp.api_library.respository.Book;
 
 import com.bootcamp.api_library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, UUID> {
+public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor {
 }
