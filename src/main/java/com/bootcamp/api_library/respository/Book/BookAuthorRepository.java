61e49f29623ec09050b8c5492c73b9e378cfe8a5
dustisme <androsrivas@outlook.com>
2024-12-16 11:46:11 +0100

    refactor book repository

---
+++ src/main/java/com/bootcamp/api_library/respository/Book/BookAuthorRepository.java
@@ -0,0 +1,11 @@
+package com.bootcamp.api_library.respository.Book;
+
+import com.bootcamp.api_library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookAuthorRepository extends JpaRepository<Book, UUID> {
+    List<Book> findBookByAuthorContaining(String author);
+}
