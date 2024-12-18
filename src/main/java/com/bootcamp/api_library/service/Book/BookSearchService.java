d8dfc766354a24cd205e57c0d39c9476eaacfeba
dustisme <androsrivas@outlook.com>
2024-12-18 12:09:41 +0100

    improve BookSearchService. Handle missing search parameters and return appropiate messages

--- src/main/java/com/bootcamp/api_library/service/Book/BookSearchService.java
+++ src/main/java/com/bootcamp/api_library/service/Book/BookSearchService.java
@@ -9,7 +9,9 @@ import com.bootcamp.api_library.specification.AuthorSearchSpecification;
 import com.bootcamp.api_library.specification.GenreSearchSpecification;
 import com.bootcamp.api_library.specification.TitleSearchSpecification;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,6 +28,11 @@ public class BookSearchService {
     }
 
     public Object searchBooks(Optional<String> title, Optional<String> author, Optional<String> genre) {
+
+        if (title.isEmpty() && author.isEmpty() && genre.isEmpty()) {
+            return new ApiResponse("Please provide at least one search parameter.");
+        }
+
         Specification<Book> specification = Specification.where(null);
 
         if (title.isPresent() && !title.get().isEmpty()) {
